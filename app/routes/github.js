2014a4c294c0adbf394a65a3f6bbf7852258cd33
vlad <vladkokul@gmail.com>
2016-06-29 13:46:48 +0300

    index

--- app/routes/github.js
+++ app/routes/github.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
-let repos = Ember.$.getJSON('https://api.github.com/users/x-formation/repos')
+let repos = Ember.$.getJSON('https://api.github.com/users/x-formation/repos');
 
 export default Ember.Route.extend({
   model(){
     return repos.then(orders => orders.sortBy('forks').reverse());
   }
+
 });
+
